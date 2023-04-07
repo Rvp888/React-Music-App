d9dd94ffd6e3eabb8a3555baa211f6c5f0f18b93
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-07 21:33:37 +0530

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Login() {
+  return (
+    <Container>
+      <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" alt="spotify" />
+      <button>Connect Spotify</button>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: #1db954;
+    gap: 5rem;
+    img {
+        height: 20vh;
+    }
+    button {
+        padding: 1rem 5rem;
+        border-radius: 5rem;
+    }
+`;
