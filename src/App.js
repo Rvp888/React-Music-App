20ac8a3ff8a2fd210393c07d746b13142c879c16
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-08 00:25:29 +0530

    modify files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Login from './components/Login';
 import { useStateProvider } from './utils/StateProvider';
 import { reducerCases } from './utils/Constants';
+import Spotify from './components/Spotify';
 
 export default function App() {
 
@@ -11,7 +12,7 @@ export default function App() {
     const hash = window.location.hash;
     if(hash) {
       const token = hash.substring(1).split("&")[0].split("=")[1];
-      dispatch({action: reducerCases.SET_TOKEN, token})
+      dispatch({type: reducerCases.SET_TOKEN, token})
     }
   },[token, dispatch]);
 
