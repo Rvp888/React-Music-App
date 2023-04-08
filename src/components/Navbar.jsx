4ebfdb24e23ae0b46405d5997ae96ee03062df8e
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-08 15:05:20 +0530

    modify files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,4 +53,29 @@ const Container = styled.div`
       }
     }
   }
+  .avatar {
+    background-color: black;
+    padding: 0.3rem 0.4rem;
+    padding-right: 1rem;
+    border-radius: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    a {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 0.5rem;
+      text-decoration: none;
+      color: white;
+      font-weight: bold;
+      svg {
+        font-size: 1.3rem;
+        background-color: #282828;
+        padding: 0.2rem;
+        border-radius: 1rem;
+        color: #c7c5c5;
+      }
+    }
+  }
 `;
