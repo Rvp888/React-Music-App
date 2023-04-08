1936dc6d89b0944624d78f2bf340493f5e8964ce
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-08 18:20:26 +0530

    modify files

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,28 +1,31 @@
-import React, { useEffect } from 'react';
-import styled from 'styled-components';
+import React, { useEffect } from "react";
+import styled from "styled-components";
 import { AiFillClockCircle } from "react-icons/ai";
-import { useStateProvider } from '../utils/StateProvider';
-import axios from 'axios';
-import { reducerCases } from '../utils/Constants';
-
-
+import { useStateProvider } from "../utils/StateProvider";
+import axios from "axios";
+import { reducerCases } from "../utils/Constants";
 
 export default function Body() {
-
-  const [{ token, selectedPlaylistId, selectedPlaylist }, dispatch] = useStateProvider();
+  const [{ token, selectedPlaylistId, selectedPlaylist }, dispatch] =
+    useStateProvider();
 
   useEffect(() => {
     const getInitialPlaylist = async () => {
-      const response = await axios.get(`https://api.spotify.com/v1/playlists/${selectedPlaylistId}`, {
-        headers: {
-          Authorization: "Bearer " + token,
-          "Content-Type": "application/json",
+      const response = await axios.get(
+        `https://api.spotify.com/v1/playlists/${selectedPlaylistId}`,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json",
+          },
         }
-      });
+      );
       const selectedPlaylist = {
         id: response.data.id,
         name: response.data.name,
-        description: response.data.description.startsWith("<a") ? "" : response.data.description,
+        description: response.data.description.startsWith("<a")
+          ? ""
+          : response.data.description,
         image: response.data.images[0].url,
         tracks: response.data.tracks.items.map(({ track }) => ({
           id: track.id,
@@ -34,51 +37,82 @@ export default function Body() {
           context_uri: track.album.uri,
           track_number: track.track_number,
         })),
-      }
-      dispatch({ type: reducerCases.SET_PLAYLIST, selectedPlaylist })
+      };
+      dispatch({ type: reducerCases.SET_PLAYLIST, selectedPlaylist });
     };
     getInitialPlaylist();
   }, [token, dispatch, selectedPlaylistId]);
 
-
-
   return (
     <Container>
-      {
-        selectedPlaylist && (
-          <>
-            <div className="playlist">
-              <div className="image">
-                <img src={selectedPlaylist.image} alt="selectedPlaylist" />
+      {selectedPlaylist && (
+        <>
+          <div className="playlist">
+            <div className="image">
+              <img src={selectedPlaylist.image} alt="selectedPlaylist" />
+            </div>
+            <div className="details">
+              <span className="type">PLAYLIST</span>
+              <h1 className="title">{selectedPlaylist.name}</h1>
+              <p className="description">{selectedPlaylist.description}</p>
+            </div>
+          </div>
+          <div className="list">
+            <div className="header_row">
+              <div className="col">
+                <span>#</span>
               </div>
-              <div className="details">
-                <span className='type'>PLAYLIST</span>
-                <h1 className='title'>{selectedPlaylist.name}</h1>
-                <p className='description'>{selectedPlaylist.description}</p>
+              <div className="col">
+                <span>TITLE</span>
+              </div>
+              <div className="col">
+                <span>ALBUM</span>
+              </div>
+              <div className="col">
+                <span>
+                  <AiFillClockCircle />
+                </span>
               </div>
             </div>
-            <div className="list">
-              <div className="header_row">
-                <div className="col">
-                  <span></span>
-                </div>
-                <div className="col">
-                  <span></span>
-                </div>
-                <div className="col">
-                  <span></span>
-                </div>
-                <div className="col">
-                  <span></span>
-                </div>
-              </div>
+            <div className="tracks">
+              {selectedPlaylist.tracks.map(
+                (
+                  {
+                    id,
+                    name,
+                    artists,
+                    image,
+                    duration,
+                    album,
+                    context_uri,
+                    track_number,
+                  },
+                  index
+                ) => {
+                  return (
+                    <div className="row" key={id}>
+                      <div className="col">
+                        <span>{index + 1}</span>
+                      </div>
+                      <div className="col detail">
+                        <div className="image">
+                          <img src={image} alt="track" />
+                        </div>
+                        <div className="info">
+                          <span className="name">{name}</span>
+                          <span>{artists}</span>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                }
+              )}
             </div>
-          </>
-        )
-      }
+          </div>
+        </>
+      )}
     </Container>
-  )
+  );
 }
 
-
-const Container  = styled.div``;
+const Container = styled.div``;
